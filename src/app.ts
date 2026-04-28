5c5b5ed04c56da635966471d052ae4a47f92af59
Slmpire <sioplightman5@gmail.com>
2026-04-28 19:37:05 +0100

    feat: add CORS support for frontend integration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import cardRouter from "./card/card.router";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/card", cardRouter);
